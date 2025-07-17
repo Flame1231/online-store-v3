1dab1e19b738e63e110d206addab79030b2d42c4
Dmitrii Pososhkov <pososhkovd@yandex.ru>
2025-07-17 15:58:21 +0500

    add inventory microservice

---
+++ inventory-microservice/src/test/java/ru/dmiit3iy/inventorymicroservice/InventoryMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dmiit3iy.inventorymicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
