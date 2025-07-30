b6002d181f8fe3d7ba17d0e7268c11f0deb666d9
Dmitrii Pososhkov <pososhkovd@yandex.ru>
2025-07-30 14:29:40 +0500

    добавил слой сервиса и репозитория

--- notification-microservice/src/main/java/ru/otus/notificationmicroservice/repository/OrderRepository.java
+++ notification-microservice/src/main/java/ru/otus/notificationmicroservice/repository/OrderRepository.java
@@ -1,7 +1,13 @@
 package ru.otus.notificationmicroservice.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.otus.notificationmicroservice.model.Order;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    Page<Order> findAllByOrder_id(Long orderId, Pageable pageable);
+
+    Page<Order> findAllByUser_id(Long userId, Pageable pageable);
 }
---
+++ notification-microservice/src/main/java/ru/otus/notificationmicroservice/service/OrderService.java
@@ -0,0 +1,12 @@
+package ru.otus.notificationmicroservice.service;
+
+import org.springframework.data.domain.Page;
+import ru.otus.notificationmicroservice.model.Order;
+
+public interface OrderService {
+    Page<Order> getAll(int page, int size, String sortBy, String direction);
+
+    Page<Order> getById(Long orderId, int page,int size, String sortBy, String direction);
+
+    Page<Order> getByUserId(Long userId, int page,int size, String sortBy, String direction);
+}
